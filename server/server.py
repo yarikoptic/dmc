8d350cea9db47641d4ab056e034d415977079d60
Michael Hanke <michael.hanke@gmail.com>
2015-10-16 18:41:30 +0200

    Adjust JSON receiver to match the new request format

--- server/server.py
+++ server/server.py
@@ -65,7 +65,7 @@ class SurveyDB(object):
     @cherrypy.tools.json_in(force=True)
     def submit(self):
         # this is the form content as decoded JSON, aka a dict
-        data = cherrypy.request.json
+        data = json.loads(cherrypy.request.json['json'])
 
         if not is_valid_submission(data):
             # XXX should be an error of some kind
